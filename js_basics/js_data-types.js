68f966dbb9266226b1bf8c328b28ae0ce56e41ab
khurram4000 <107985762+khurram4000@users.noreply.github.com>
2024-09-12 09:42:26 +0000

    container config done

---
+++ js_basics/js_data-types.js
@@ -0,0 +1,14 @@
+"use strict";
+
+let name = "abeeha" //string data type//
+let age = 16 //numberr//
+let isloggedin = true //boolean type//
+
+                        "types"
+// bigint
+// boolean type => true or false
+// string
+// number => range is from 2 to power 53
+//null => standalone value
+// undefined =>
+// symbol => uniquee
